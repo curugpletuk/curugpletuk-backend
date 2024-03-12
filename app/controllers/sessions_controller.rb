9806c1d55811b38272f56feced9b4fc63d6a50f8
tangguhw <tangguhwidodo42.com>
2024-03-12 20:43:27 +0700

    add revision of sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
   def destroy
     access_token = request.headers['Authorization']
     result = Session.destroy_session(access_token)
-    render json: { status: result[:status], message: result[:message] }, status: result[:status]
+    # render json: { code: result[:code], status: result[:status], message: result[:message] }, status: result[:status]
+    default_response(result)
   end
 
   private
