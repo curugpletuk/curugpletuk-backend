3f5e86a926484f4bde09153429193d63b3a1f4df
tangguhw <tangguhwidodo42.com>
2024-03-14 23:55:05 +0700

    add revision in docker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.redis = { url: ENV['REDIS_URL'] }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
